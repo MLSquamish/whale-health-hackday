2c5e2db7b6e051b9cc77fc997fab5c4bba101aa8
peter <peter.ed.oconnor@gmail.com>
2024-06-26 21:58:12 -0700

    pushed more segmenters

--- whale_health/demo_segment_anything.py
+++ whale_health/demo_segment_anything.py
@@ -4,7 +4,7 @@ import cv2
 
 from whale_health.visualization_tools import get_mask_image
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
 
 
 """
@@ -56,6 +56,35 @@ class KMColorSegmenter(ISegmenter):
         return masks
 
 
+class HandCodedColorSegmenter(ISegmenter):
+    """
+    We are looking for greyish whale colour, without the blue of the water or the white of the reflection.
+    """
+
+    def segment(self, image: np.ndarray) -> np.ndarray:
+        h, w, _ = image.shape
+        masks = np.zeros((2, h, w), dtype=bool)
+        masks[1] = np.abs(image[:, :, 0] - image[:, :, 1]) < 10
+        masks[1][image.mean(axis=2) > 175] = False
+        # masks[1] = np.abs(image[:, :, 0] - image[:, :, 2]) < 10
+        # masks[2] = np.abs(image[:, :, 1] - image[:, :, 2]) < 10
+        masks[0] = np.logical_not(np.any(masks[1:], axis=0))
+        return masks
+#
+#
+# def locally_normalize_image(image: np.ndarray, box_filter_relative_width: float = 0.2) -> np.ndarray:
+#     """
+#     Locally normalize an image by subtracting the local mean and dividing by the local standard deviation.
+#     :param image: A BGR image as a numpy array
+#     :param box_filter_relative_width: The relative width of the box filter used to compute the local mean and std
+#     :return: The locally normalized image
+#     """
+#     h, w, _ = image.shape
+#     box_filter_width = int(box_filter_relative_width * min(h, w))
+#     image = image.astype(np.float32)
+#     mean = cv2.boxFilter(image, -1, (box_filter_width, box_filter_width), normalize=False)
+
+
 
 
 
@@ -65,7 +94,11 @@ def demo_segment_anything(
     image = cv2.imread(image_path)
 
     # Resize to 800x600
-    image = cv2.resize(image, (800, 600))
+    # image = cv2.resize(image, (640, 480))
+    image = cv2.resize(image, (1920, 1080))
+
+    # image = locally_normalize_image(image)
+
 
     # sam = sam_model_registry["vit_h"]()
     # predictor = SamPredictor(sam)
@@ -73,7 +106,9 @@ def demo_segment_anything(
     # print("Segmenting Whale...")
     # masks, a, b = predictor.predict()
 
-    masks = KMColorSegmenter(n_colors=2).segment(image)
+    # masks = KMColorSegmenter(n_colors=2).segment(image)
+    masks = HandCodedColorSegmenter().segment(image)
+    # masks = DBScanColorSegmenter(eps=0.5, min_samples=5).segment(image)
 
 
     cv2.imshow('Raw', image)
